094cf9753557fd00d5d42e0e398307cb54c75e73
Sander-317 <sander.van.meerveld@gmail.com>
2022-12-20 16:21:24 +0100

    added state to the list

--- src/components/List.js
+++ src/components/List.js
@@ -2,14 +2,36 @@ import React from "react";
 import ListItem from "./ListItem";
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceryItems: [
+        { id: 1, title: "steak" },
+        { id: 2, title: "rice" },
+        { id: 3, title: "coke" },
+        { id: 4, title: "chips" },
+      ],
+    };
+    this.listHandler = this.listHandler.bind(this);
+  }
+
+  listHandler() {
+    const newArray = this.state.groceryItems.map((item) => {
+      return <ListItem id={item.id} title={item.title} />;
+    });
+    console.log(newArray);
+    return newArray;
+  }
+
   render() {
     return (
       <div>
         <ul>
-          <ListItem title="steak" />
+          {this.listHandler()}
+          {/* <ListItem title="steak" />
           <ListItem title="rice" />
           <ListItem title="coke" />
-          <ListItem title="chips" />
+          <ListItem title="chips" /> */}
         </ul>
       </div>
     );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function ListItem(props) {
   console.log(props);
-  return <li>{props.title}</li>;
+  return (
+    <li
+      key={props.id}
+      className="list-item"
+      //   onClick={clickItem}
+      value={props.title}
+    >
+      {props.title}
+    </li>
+  );
 }
 
 export default ListItem;
