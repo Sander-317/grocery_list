b0bcee223d3e2f9361aba2d07f41e82448c1917f
Sander-317 <sander.van.meerveld@gmail.com>
2022-12-22 15:00:40 +0100

    moved state from list to container

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,18 @@ import ShoppingCart from "./ShoppingCart";
 import GroceryList from "./GroceryList";
 
 class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceryItems: [
+        { id: 1, title: "steak" },
+        { id: 2, title: "rice" },
+        { id: 3, title: "coke" },
+        { id: 4, title: "chips" },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
--- src/components/List.js
+++ src/components/List.js
@@ -2,17 +2,17 @@ import React from "react";
 import ListItem from "./ListItem";
 
 class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      groceryItems: [
-        { id: 1, title: "steak" },
-        { id: 2, title: "rice" },
-        { id: 3, title: "coke" },
-        { id: 4, title: "chips" },
-      ],
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     groceryItems: [
+  //       { id: 1, title: "steak" },
+  //       { id: 2, title: "rice" },
+  //       { id: 3, title: "coke" },
+  //       { id: 4, title: "chips" },
+  //     ],
+  //   };
+  // }
 
   onItemClick(item) {
     console.log(`Clicked ${item.id}: ${item.title}`);
